f7bfca96be35e964df2b18ccda97f81a10cf328a
Aayushi <aayu.shah711@gmail.com>
2020-12-03 14:26:20 +0530

    Added dummmy learning resources

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <learning-resource-item
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource-item>
+  </ul>
+</template>
+
+<script>
+import LearningResourceItem from './LearningResourceItem.vue';
+export default {
+  components: { LearningResourceItem },
+  props: ['resources']
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
